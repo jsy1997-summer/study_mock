9b5d3e34e1615ada0c9781d9c22001b34c2e465d
贾素银 <jiasy@sincinfo.cn>
2022-08-28 23:38:31 +0800

    学习mock.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import '../src/mock/mock'
+import axios from 'axios'
+
 
 function App() {
+  function testclick() {
+    axios.get('/getdata1', {
+      params: {
+        id: '123'
+      }
+    })
+    // axios({
+    //   type: 'post',
+    //   url: '/getdata',
+    //   data: {
+    //     'data': '123456'
+    //   },
+    //   dataType: 'jsonp',
+    //   header: {
+    //     'Content-Type': 'application/json;charset=utf-8'  //如果写成contentType会报错
+    //   }
+    // })
+    //   .then(res => {
+    //     console.log(res, 'res')
+    //   })
+    //   .catch(Error => {
+    //     console.log(Error)
+    //   })
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={testclick}>点击</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
