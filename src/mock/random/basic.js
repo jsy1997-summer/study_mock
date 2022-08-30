019ad68117e5164bfb60d3487e9fed70d93d3b0c
jsy <1175341936@qq.com>
2022-08-30 15:24:37 +0800

    'mock和setup'

---
+++ src/mock/random/basic.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: jiasuyin
+ * @Date: 2022-08-30 13:36:04
+ * @LastEditors: jiasuyin
+ * @LastEditTime: 2022-08-30 13:47:40
+ * @important: 重要提醒
+ * @Description: 备注内容
+ * @FilePath: \study_mock\src\mock\random\basic.js
+ */
+import Mock from 'mockjs'
+const random = Mock.Random()
+export default Mock.mock('/basic','post',)
