0ff56634ec114efc10647d3f5ebe0febd5d2eee3
jsy <1175341936@qq.com>
2022-09-27 10:28:09 +0800

    '测试'

---
+++ src/views/test2/test.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: jiasuyin
+ * @Date: 2022-09-27 10:26:13
+ * @LastEditors: jiasuyin
+ * @LastEditTime: 2022-09-27 10:26:24
+ * @important: 重要提醒
+ * @Description: 备注内容
+ * @FilePath: \study_mock\src\views\test2\test.js
+ */
+function test(){}
